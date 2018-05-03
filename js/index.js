48f05fea32bb2201460cc76cdbb2de34758ab1e7
MikeDurhamShef <34306498+MikeDurhamShef@users.noreply.github.com>
2018-05-03 12:21:47 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//Wow JS
+new WOW().init();
+
+//AOS JS
+AOS.init();
+
+
+
+// Slick Slider carousel
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    arrows: true,
+    adaptiveHeight: true,
+   adaptiveWidth: true,
+  });
+
+  $('.caption_slides').slick({
+    dots: true
+  });
+
+
+});
